f6295d7c4a68dc27b36e64bd1c80bc7447cf3f37
New Account Name <minhazul.hasan.sohan@gmail.com>
2023-03-12 20:35:07 +0600

    background image add

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -17,10 +17,10 @@ const StoreItem = ({ id, name, price, imgUrl }: StoreItemProps) => {
 
     return (
         <Card className='h-100'>
-            <Card.Img variant='top' src={imgUrl} height="200px" style={{ objectFit: "cover" }} />
+            <Card.Img variant='top' src={imgUrl} height="400px" style={{ objectFit: "cover" }} />
             <Card.Body className='d-flex flex-column'>
                 <Card.Title className='d-flex justify-content-between align-items-baseline mb-4'>
-                    <span className='fs-2'>{name}</span>
+                    <span className='fs-4'>{name}</span>
                     <span className='ms-2 text-muted'>{formatCurrency(price)}</span>
                 </Card.Title>
                 <div className="mt-auto">
